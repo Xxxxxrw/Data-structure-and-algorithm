16274492aaa546fc503f08eeafa732403d3bf34f
XRW <3218127495@qq.com>
2023-01-14 21:04:32 +0800

    second

--- 3.BinTree/testcpp.cpp
+++ 3.BinTree/testcpp.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 
-#include <string> 
-
 using namespace std;
-string a[10];
-bool cmp(string x,string y)
-{
-	
-	cout << x << " " << y << endl;
-	printf("hello");
-	return 1;
-}
+
 int main()
 {
-	cin>>a[0]>>a[1];
-	cmp(a[0],a[1]);
-	
+	int a[10005];
+	int b[10005];
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int top = 0;
+    for(int i = n - 1; i >= 0; i--)
+    {
+        for(int j = 0; j < top; j++)
+        {
+            if(b[j] == a[i])
+            {
+                continue;
+            }
+            b[top++] = a[i];
+        }
+    }
+    cout << top;
+    if(top > 0)
+    {
+        for(int i = top-1; i > 0; i++)
+        {
+           cout << b[i] << " ";
+        }
+    }
     return 0;
 }
--- 3.BinTree/笔记2.cpp
+++ 3.BinTree/笔记2.cpp
@@ -2,3 +2,17 @@
 //abs();对整数取绝对值   fabs();对浮点型取绝对值 头文件math 或者 stdlib
 
 //min(a,b) 返回a,b中的较小者，大于两个参数可以多次嵌套使用 头文件algorithm 
+
+//map<string,int> dict 定义映射。赋值的方式：
+//1.dict.insert(made_pair("Tom",1)); pair会将Tom映射到1，需要引头文件Utility
+//	dict.insert(pair<string,int>("Tom",1));
+//2.dict.insert(map<string,int>::value_type("Tom",1));
+//3.dict["Tom"] = 1; 第三种方式可以覆盖值
+//判断是否生成映射：dict.count("Tom"); 是返回1，否则返回0
+//遍历map是按照关键字从小到大遍历的。
+//map<string,int>::iterator it
+
+//string 不可以给 char [] 赋值; 但char[]可以给 string 赋值 
+//用scanf输入string形式的数据：string a; scanf("%s",&a[0]); 
+//弊端：无法使用a.length();a.size();reverse等功能; 用char[]给string赋值则可正常使用
+//用printf输出string:  printf("%s",a.c_str()); 
